e81e210964c9464552f41a62e7dc7bdf7e580c59
Eugene <eugene.voronov.1990@mail.ru>
2024-04-24 20:17:47 +0300

    4

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -15,23 +15,36 @@ public class GuessNumber {
         Player currentPlayer = player1;
         Scanner scanner = new Scanner(System.in);
         int inputNumber = 0;
-        while (randomNumber != inputNumber) {
+        while (true) {
             System.out.print(currentPlayer.getName() + ", введите ваше предполагаемое число: ");
-            inputNumber = scanner.nextInt();
+            inputNumber = enterNumber(scanner);
 
             if (inputNumber == randomNumber) {
                 System.out.println("Поздравляем, " + currentPlayer.getName() + ", вы победили!");
                 break;
-            } else if (inputNumber > randomNumber) {
-                System.out.println("Число " + inputNumber + " больше загаданного числа.");
-            } else {
-                System.out.println("Число " + inputNumber + " меньше загаданного числа.");
             }
-            if (currentPlayer == player1) {
-                currentPlayer = player2;
-            } else {
-                currentPlayer = player1;
-            }
+            checkNumber(inputNumber);
+            currentPlayer = switchPlayer(currentPlayer);
+        }
+    }
+
+    public int enterNumber(Scanner scanner) {
+        return scanner.nextInt();
+    }
+
+    public void checkNumber(int inputNumber) {
+        if (inputNumber > randomNumber) {
+            System.out.println("Число " + inputNumber + " больше загаданного числа.");
+        } else {
+            System.out.println("Число " + inputNumber + " меньше загаданного числа.");
+        }
+    }
+
+    public Player switchPlayer(Player currentPlayer) {
+        if (currentPlayer == player1) {
+            return currentPlayer = player2;
+        } else {
+            return currentPlayer = player1;
         }
     }
 }
