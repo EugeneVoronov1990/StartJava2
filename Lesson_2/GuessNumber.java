93dc991aafcb5a7bdf6f38158dd7ba034302e1bd
Eugene <eugene.voronov.1990@mail.ru>
2024-04-26 19:38:44 +0300

    5

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -8,43 +8,34 @@ public class GuessNumber {
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
-        this.randomNumber = (int) (Math.random() * 100) + 1;
+        randomNumber = (int) (Math.random() * 100) + 1;
     }
 
     public void play() {
         Player currentPlayer = player1;
         Scanner scanner = new Scanner(System.in);
         int inputNumber = 0;
-        while (true) {
-            System.out.print(currentPlayer.getName() + ", введите ваше предполагаемое число: ");
-            inputNumber = enterNumber(scanner);
+        do {
+            inputNumber = enterNumber(currentPlayer, scanner);
 
-            if (inputNumber == randomNumber) {
-                System.out.println("Поздравляем, " + currentPlayer.getName() + ", вы победили!");
-                break;
-            }
-            checkNumber(inputNumber);
+            checkNumber(inputNumber, randomNumber);
+
             currentPlayer = switchPlayer(currentPlayer);
-        }
+        } while (inputNumber != randomNumber);
+        System.out.println("Поздравляем, " + currentPlayer.getName() + ", вы победили!");
     }
-
-    public int enterNumber(Scanner scanner) {
+    private int enterNumber( Player currentPlayer, Scanner scanner) {
+        System.out.print(currentPlayer.getName() + ", введите ваше предполагаемое число: ");
         return scanner.nextInt();
     }
-
-    public void checkNumber(int inputNumber) {
+    public void checkNumber(int inputNumber, int randomNumber) {
         if (inputNumber > randomNumber) {
             System.out.println("Число " + inputNumber + " больше загаданного числа.");
         } else {
             System.out.println("Число " + inputNumber + " меньше загаданного числа.");
         }
     }
-
     public Player switchPlayer(Player currentPlayer) {
-        if (currentPlayer == player1) {
-            return currentPlayer = player2;
-        } else {
-            return currentPlayer = player1;
-        }
+        return currentPlayer == player1 ? player2 : player1;
     }
 }
